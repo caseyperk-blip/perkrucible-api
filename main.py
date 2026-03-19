365692ce763417ee057db1d048e3637c0c34b4fa
Casey Perkins <caseyperk@gmail.com>
2026-03-19 13:42:24 -0600

    lazy load rembg to reduce render startup issues

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import Response
 from fastapi.middleware.cors import CORSMiddleware
-from rembg import remove
 
 app = FastAPI()
 
@@ -23,6 +22,8 @@ async def root():
 
 @app.post("/remove-bg")
 async def remove_background(file: UploadFile = File(...)):
+    from rembg import remove
+
     input_bytes = await file.read()
     output_bytes = remove(input_bytes)
     return Response(content=output_bytes, media_type="image/png")
